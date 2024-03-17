1675420b442f00d29c3cfc57d889c51a592db7d7
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-03-17 16:38:38 +0300

    scss body padding ve margin 0 yapıldı ve gerekli dosyalar indirildi

--- GbFrontend/src/App.jsx
+++ GbFrontend/src/App.jsx
@@ -1,9 +1,11 @@
 import Login from "./pages/login/Login";
 
 function App() {
-  return <div>
-    <Login/>
-  </div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
 
 export default App;
