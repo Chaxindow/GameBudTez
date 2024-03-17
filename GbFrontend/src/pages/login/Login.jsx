acabe6afe4b2678565a01957425cc59b3e9a2f60
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-03-17 17:34:00 +0300

    Router added for register and login pages

--- GbFrontend/src/pages/login/Login.jsx
+++ GbFrontend/src/pages/login/Login.jsx
@@ -1,27 +1,30 @@
-import "./login.scss"
+import { Link } from "react-router-dom";
+import "./login.scss";
 
 const Login = () => {
   return (
     <div className="login">
-        <div>
-            <div className="card">
-                <div className="left">
-                    <h1>GAMEBUD</h1>
-                    <span>Don't you have an account?</span>
-                    <button>Register</button>
-                </div>
-                <div className="right">
-                        <h1>Login</h1>
-                        <form>
-                            <input type="text" placeholder="Username" />
-                            <input type="text" placeholder="Password" />
-                            <button>Login</button>
-                        </form>
-                    </div>
-            </div>
+      <div>
+        <div className="card">
+          <div className="left">
+            <h1>GAMEBUD</h1>
+            <span>Don't you have an account?</span>
+            <Link to="/register">
+              <button>Register</button>
+            </Link>
+          </div>
+          <div className="right">
+            <h1>Login</h1>
+            <form>
+              <input type="text" placeholder="Username" />
+              <input type="text" placeholder="Password" />
+              <button>Login</button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- GbFrontend/src/pages/register/Register.jsx
+++ GbFrontend/src/pages/register/Register.jsx
@@ -1,33 +1,32 @@
-import { Link } from "react-router-dom"
-import "./register.scss"
+import { Link } from "react-router-dom";
+import "./register.scss";
 
 const Register = () => {
   return (
     <div className="register">
-        <div>
-            <div className="card">
-                <div className="left">
-                    <h1>GAMEBUD</h1>
-                    <span>Do you have an account?</span>
-                    <Link to="/login">
-                    <button>Login</button>
-                    </Link>
-                    
-                </div>
-                <div className="right">
-                        <h1>Register</h1>
-                        <form>
-                            <input type="text" placeholder="Username" />
-                            <input type="email" placeholder="Email" />
-                            <input type="password" placeholder="Password" />
-                            <input type="text" placeholder="Name" />
-                            <button>Register</button>
-                        </form>
-                    </div>
-            </div>
+      <div>
+        <div className="card">
+          <div className="left">
+            <h1>GAMEBUD</h1>
+            <span>Do you have an account?</span>
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          </div>
+          <div className="right">
+            <h1>Register</h1>
+            <form>
+              <input type="text" placeholder="Username" />
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <input type="text" placeholder="Name" />
+              <button>Register</button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
