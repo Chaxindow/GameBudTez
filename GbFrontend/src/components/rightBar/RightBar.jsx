ecd0f068f307802e2a8b59d173b902e3769ecb96
yigitsaglam3 <lyigitsaglamm@gmail.com>
2024-03-23 10:51:59 +0300

    barlar sticky yapıldı rightbar eklendi

--- GbFrontend/src/components/rightBar/RightBar.jsx
+++ GbFrontend/src/components/rightBar/RightBar.jsx
@@ -1,7 +1,96 @@
 import "./rightBar.scss";
 
 const RightBar = () => {
-  return <div className="rightBar">RightBar</div>;
+  return <div className="rightBar">
+    <div className="container">
+      <div className="item">
+        <span>Tanıyor olabileceğin kişiler</span>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+            <span>Mete Çalışkan</span>
+          </div>
+          <div className="buttons">
+            <button>takip et</button>
+            <button>reddet</button>
+          </div>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+            <span>Mete Çalışkan</span>
+          </div>
+          <div className="buttons">
+            <button>takip et</button>
+            <button>reddet</button>
+          </div>
+        </div>
+      </div>
+      <div className="item">
+        <span>Son Hareketler</span>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+            <p>
+              <span>Mete Çalışkan </span> profil fotoğrafını güncelledi
+            </p>
+          </div>
+          <span>1 dk önce</span>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+            <p>
+              <span>Mete Çalışkan </span> profil fotoğrafını güncelledi
+            </p>
+          </div>
+          <span>1 dk önce</span>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+            <p>
+              <span>Mete Çalışkan </span> profil fotoğrafını güncelledi
+            </p>
+          </div>
+          <span>1 dk önce</span>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+            <p>
+              <span>Mete Çalışkan </span> profil fotoğrafını güncelledi
+            </p>
+          </div>
+          <span>1 dk önce</span>
+        </div>
+      </div>
+      <div className="item">
+        <span>Çevrimiçi</span>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+              <div className="online"/>
+              <span>Mete Çalışkan </span>
+          </div>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+              <div className="online"/>
+              <span>Mete Çalışkan </span>
+          </div>
+        </div>
+        <div className="user">
+          <div className="userInfo">
+            <img src="/images/metepp.jpg" alt="" />
+              <div className="online"/>
+              <span>Mete Çalışkan </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default RightBar;
