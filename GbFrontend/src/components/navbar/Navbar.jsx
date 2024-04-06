d4e869f1271c28df00d535218c8c6d4bbe5b6571
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-04-06 17:59:49 +0300

    dark icon added cursor pointer

--- GbFrontend/src/components/navbar/Navbar.jsx
+++ GbFrontend/src/components/navbar/Navbar.jsx
@@ -7,55 +7,55 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
-import StoreOutlinedIcon from '@mui/icons-material/StoreOutlined';
-import { Link } from 'react-router-dom';
+import StoreOutlinedIcon from "@mui/icons-material/StoreOutlined";
+import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
 
 const Navbar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
 
-  const {toggle, darkMode} = useContext(DarkModeContext);
-  const {currentUser} = useContext(AuthContext);
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>GameBud</span>
+        </Link>
 
-  return <div className="navbar">
-    <div className="left">
-      <Link to="/" style={{textDecoration: "none"}}>
-        <span>GameBud</span> 
-      </Link>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <HomeOutlinedIcon style={{ color: "inherit", marginTop: "6px" }} />
+        </Link>
 
-      <Link to="/" style={{textDecoration:"none", color:"inherit" }}>
-        <HomeOutlinedIcon style={{color: "inherit", marginTop:"6px"}}/>
-      </Link>
-        
         {darkMode ? (
-        <WbSunnyOutlinedIcon onClick={toggle}/>
-        ): (
-        <DarkModeOutlinedIcon onClick={toggle}/>
+          <WbSunnyOutlinedIcon onClick={toggle} cursor="pointer" />
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} cursor="pointer" />
         )}
-        <GridViewOutlinedIcon/>
-        <StoreOutlinedIcon/>
+        <GridViewOutlinedIcon />
+        <StoreOutlinedIcon />
         <div className="search">
-          <SearchOutlinedIcon/>
+          <SearchOutlinedIcon />
           <input type="text" placeholder="Bul beni..." />
         </div>
-      
-      
-    </div>
-    <div className="right">
-      <PersonOutlinedIcon/>
-      <EmailOutlinedIcon/>
-      <NotificationsOutlinedIcon/>
-      <div className="user">
-        <img src={currentUser.profilePic} alt="" />
-          <Link to={`/profile/${currentUser.userId}`}style={{textDecoration:"none",color:"inherit" }}>
-          <span>{currentUser.name}</span>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src={currentUser.profilePic} alt="" />
+          <Link
+            to={`/profile/${currentUser.userId}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <span>{currentUser.name}</span>
           </Link>
-        
-
+        </div>
       </div>
     </div>
-    </div>;
+  );
 };
 
 export default Navbar;
