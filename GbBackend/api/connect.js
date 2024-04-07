ae6836b4f655f0c64c1e163a90b0cad378ddbee2
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-04-07 14:58:04 +0300

    Server bağlanıtıs dışında kodlar oluştu

---
+++ GbBackend/api/connect.js
@@ -0,0 +1,9 @@
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "1234",
+  database: "gamebud",
+});
--- GbBackend/api/index.js
+++ GbBackend/api/index.js
@@ -1,5 +1,20 @@
-import Express from "express";
-const app = Express();
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/comments", commentRoutes);
+app.use("/api/v1/likes", likeRoutes);
 
 app.listen(8800, () => {
   console.log("API Works");
