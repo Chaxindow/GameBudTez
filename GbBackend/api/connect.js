0d32800f5f5a779204d013809d43e9f5aabdb6f7
yigitsaglam3 <lyigitsaglamm@gmail.com>
2024-04-08 12:55:13 +0300

    server çözüldü dbye bağlanırken şifreni değiştirmeyi unutma ok kib bye

--- GbBackend/api/connect.js
+++ GbBackend/api/connect.js
@@ -4,6 +4,23 @@ export const db = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "1234",
-  database: "gamebud",
+  password: "Doggerygt1907.",
+  database: "social",
 });
+
+
+// MySQL sunucusuna bağlanma işlemi
+db.connect(function (err) {
+  if (err) {
+    console.error("MySQL sunucusuna bağlanma hatas: " + err.stack);
+    return;
+  }
+
+  console.log("MySQL sunucusuna başaryla bağlan. Bağlant ID: " + db.threadId);
+});
+
+// MySQL sunucusundan gelen hataları ele alma
+db.on("error", function (err) {
+  console.error("Beklenmeyen MySQL bağlant hatas: " + err.code);
+});
+
