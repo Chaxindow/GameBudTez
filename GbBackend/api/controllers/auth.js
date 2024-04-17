8b4fdbcd7ad43bd3b5eabb6a01e417bb1a7f9cb1
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-04-17 18:47:40 +0300

    auth.js functions are complated

--- GbBackend/api/controllers/auth.js
+++ GbBackend/api/controllers/auth.js
@@ -1,5 +1,9 @@
+import { json } from "express";
 import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// mail karşılaştırılması yapılabilir
 
 export const register = (req, res) => {
   //CHECK USER IF EXISTS
@@ -8,7 +12,11 @@ export const register = (req, res) => {
 
   db.query(q, [req.body.username], (err, data) => {
     if (err) return res.status(500).json(err);
-    if (data.length) return res.status(409).json("User already exists!");
+    if (data.length) {
+      console.log(data.length);
+      console.log(data);
+      return res.status(409).json("User already exists!");
+    }
     //CREATE A NEW USER
     //Hash the password
     const salt = bcrypt.genSaltSync(10);
@@ -31,6 +39,40 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  const q = "SELECT * FROM users WHERE username = ?";
 
-export const logout = (req, res) => {};
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500), json(err);
+    if (data.length === 0) return res.status(404), json("User not found !!");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong password or username");
+
+    const token = jwt.sign({ id: data[0].id }, "secretkey"); // env dosyası ile bunu koruyabilirsin
+
+    const { password, ...others } = data[0];
+
+    res
+      .cookie("accesToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
+};
+
+export const logout = (req, res) => {
+  res
+    .clearCookie("accesToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
--- GbBackend/api/index.js
+++ GbBackend/api/index.js
@@ -4,11 +4,15 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
