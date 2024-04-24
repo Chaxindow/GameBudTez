c012c8ac17e6e16b26b9bc8f3ea76b243e1cd434
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-04-24 16:25:59 +0300

    share olustuldu ve kullanima hazır

---
+++ GbFrontend/src/components/share/Share.jsx
@@ -0,0 +1,81 @@
+import "./share.scss";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+
+const Share = () => {
+  const [file, setFile] = useState(null);
+  const [desc, setDesc] = useState("");
+
+  const { currentUser } = useContext(AuthContext);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (newPost) => {
+      return makeRequest.post("/posts", newPost);
+    },
+    onSuccess: async () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc });
+  };
+
+  return (
+    <div className="share">
+      <div className="container">
+        <div className="top">
+          <div className="left">
+            <img src={currentUser.profilePic} alt="" />
+            <input
+              type="text"
+              placeholder={`What's on your mind ${currentUser.name} ?`}
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <div className="right">
+            {file && (
+              <img className="file" alt="" src={URL.createObjectURL(file)} />
+            )}
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <input
+              type="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <label htmlFor="file">
+              <div className="item">
+                <img src="" alt="" />
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className="item">
+              <img src="" alt="" />
+              <span>Add Place</span>
+            </div>
+            <div className="item">
+              <img src="" alt="" />
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className="right">
+            <button onClick={handleClick}>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
--- GbFrontend/src/pages/home/Home.jsx
+++ GbFrontend/src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
-import Stories from "../../components/stories/Stories"
-import Posts from "../../components/posts/Posts"
-import "./home.scss"
+import Stories from "../../components/stories/Stories";
+import Posts from "../../components/posts/Posts";
+import Share from "../../components/share/Share";
+import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <Stories/>
-      <Posts/>
+      <Stories />
+      <Share />
+      <Posts />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
