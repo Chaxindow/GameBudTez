f938f2c532961ac24000483aeed2a82bb6fdc0f1
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2024-04-26 17:38:44 +0300

    likes created

--- GbFrontend/src/components/post/Post.jsx
+++ GbFrontend/src/components/post/Post.jsx
@@ -5,14 +5,40 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { makeRequest } from "../../axios";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useContext, useState } from "react";
 import moment from "moment";
 import Comments from "../comments/Comments";
+import { AuthContext } from "../../context/authContext";
 
 const Post = ({ post }) => {
+  const { currentUser } = useContext(AuthContext);
   const [commentOpen, setCommentOpen] = useState(false);
-  //TEMP
-  const liked = false;
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["likes", post.id],
+    queryFn: () =>
+      makeRequest.get("/likes?postId=" + post.id).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (liked) => {
+      if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+      return makeRequest.post("/likes", { postId: post.id });
+    },
+    onSuccess: async () => {
+      queryClient.invalidateQueries(["likes"]);
+    },
+  });
+
+  const handleLike = () => {
+    mutation.mutate(data.includes(currentUser.id));
+  };
 
   return (
     <div className="post">
@@ -38,8 +64,17 @@ const Post = ({ post }) => {
         </div>
         <div className="info">
           <div className="item">
-            {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            23 Beğeni
+            {isLoading ? (
+              "loading"
+            ) : data.includes(currentUser.id) ? (
+              <FavoriteOutlinedIcon
+                style={{ color: "red" }}
+                onClick={handleLike}
+              />
+            ) : (
+              <FavoriteBorderOutlinedIcon onClick={handleLike} />
+            )}
+            {data?.length} Likes
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
--- GbFrontend/src/components/posts/Posts.jsx
+++ GbFrontend/src/components/posts/Posts.jsx
@@ -12,8 +12,6 @@ const Posts = () => {
       }),
   });
 
-  console.log(data);
-
   return (
     <div className="posts">
       {error
