484891ebe112156a6099b4a10ab5febaa1f09917
yigitsaglam3 <lyigitsaglamm@gmail.com>
2024-05-15 12:55:10 +0300

    announcement ve slider tamam kategorilerden devam edilecek

--- GbFrontend/src/pages/StoreHome.jsx
+++ GbFrontend/src/pages/StoreHome.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import StoreNavbar from '../components/StoreNavbar';
+import Announcement from '../components/Announcement';
+import Slider from '../components/Slider';
 
 const StoreHome = () => {
   return (
     <div>
+        <Announcement/>
         <StoreNavbar/>
+        <Slider/>
+
     </div>
   )
 }
